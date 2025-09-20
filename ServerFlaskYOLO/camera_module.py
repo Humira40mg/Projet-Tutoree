5b09b0fb0d47c37f7ebe9e85554d1546c8c222c9
Nathan MILLOT <nathanmillot@icloud.com>
2025-09-20 17:02:24 +1100

    Ajout Annexes

---
+++ ServerFlaskYOLO/camera_module.py
@@ -0,0 +1,16 @@
+"""
+Module permetant de récupérer une image prise par la camera en direct
+
+Le main est : stream3.py
+"""
+from picamera2 import Picamera2
+import cv2
+
+class Camera:
+    def __init__(self):
+        self.picam2 = Picamera2()
+        self.picam2.start()
+
+    def get_frame(self):
+        frame = self.picam2.capture_array()
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
--- autotest.sh
+++ autotest.sh
@@ -10,17 +10,17 @@ echo "[🚀] Lancement de src/main.py"
 python3 TestCase/src/main.py
 
 # Pause de 500 secondes
-echo "[⏳] Attente de 500 secondes..."
+echo "Attente de 500 secondes..."
 sleep 500
 
 # Mise à jour de la clé "model"
 jq '.model = "gemma3:4b"' "$JSON_FILE" > tmp.json && mv tmp.json "$JSON_FILE"
-echo "[✔] Modèle changé en gemma3:4b dans $JSON_FILE"
+echo "Modèle changé en gemma3:4b dans $JSON_FILE"
 
 # Pause de 500 secondes
-echo "[⏳] Attente de 500 secondes..."
+echo "Attente de 500 secondes..."
 sleep 500
 
 # Lancer le script Python
-echo "[🚀] Lancement de src/main.py"
+echo "Lancement de src/main.py"
 python3 TestCase/src/main.py
